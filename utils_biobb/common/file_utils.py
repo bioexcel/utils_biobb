901c189e166667a924a30275ea0095dfbcc480e3
Pau Andrio <andriopau@gmail.com>
2021-03-12 01:47:47 +0100

    Adding pycompss generator

--- utils_biobb/common/file_utils.py
+++ utils_biobb/common/file_utils.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import importlib
 
 
 def get_file_path_list(dir_path=None, extension="json", remove_dir_name=True, exclude_test_dir=True):
@@ -24,7 +25,24 @@ def get_file_path_list(dir_path=None, extension="json", remove_dir_name=True, ex
     return list_paths
 
 
+def _iterate_modules(current_module, paths_dict):
 
+    # current_module is a package
+    if getattr(current_module, '__all__', None):
+        for module_name in current_module.__all__:
+            module_name = importlib.import_module(current_module.__name__+'.'+module_name)
+            _iterate_modules(module_name, paths_dict)
+
+    # current_module is a module
+    else:
+        paths_dict[current_module.__name__.split('.')[-1]] = current_module.__package__.replace('.', '/')
+
+
+def get_sub_paths_dict(biobb_name):
+    current_module = __import__(biobb_name)
+    paths_dict = {}
+    _iterate_modules(current_module, paths_dict)
+    return paths_dict
 
 
 
---
+++ utils_biobb/pycompss/pycompss_generator.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import importlib
+import argparse
+import utils_biobb.common.file_utils as fu
+import json
+import jinja2
+
+
+
+class PycompssGenerator:
+
+    def __init__(self, package, output_dir):
+        self.package_name = package
+        self.output_dir = output_dir
+        try:
+            self.imported_package = importlib.import_module(package)
+        except ImportError as ie:
+            raise ie(f'{package} and biobb_adapters must be available in your environment')
+
+    def launch(self):
+        sub_paths_dict = fu.get_sub_paths_dict(biobb_name=self.package_name)
+        for module_json in fu.get_file_path_list(dir_path=self.imported_package.__path__[0]):
+            module_info = {}
+            with open(module_json) as f_json:
+                json_dict = json.load(f_json)
+                module_name = module_json.name.rsplit('.')[0]
+                module_info['iclass']= json_dict.get('name').split()[-1]
+                module_info['module_dot_path'] = sub_paths_dict.get(module_name).replace('/', '.')+'.'+module_name
+                module_info['mpi'] = json_dict.get('info').get('wrapped_software').get('multinode')
+                module_info['required'] = json_dict.get('required')
+                module_info['arguments'] = []
+                not_required = []
+                for argument, value in json_dict.get('properties').items():
+                    if argument == 'properties':
+                        continue
+                    if value.get('filetype').lower() == 'input':
+                        if argument in module_info['required']:
+                            module_info['arguments'].append((argument, 'FILE_IN'))
+                        else:
+                            not_required.append((argument, 'FILE_IN'))
+                    else:
+                        if argument in module_info['required']:
+                            module_info['arguments'].append((argument, 'FILE_OUT'))
+                        else:
+                            not_required.append((argument, 'FILE_IN'))
+
+                module_info['arguments'].extend(not_required)
+
+                adapter_dir_path = Path(self.output_dir).joinpath(Path(sub_paths_dict.get(module_name)))
+                adapter_dir_path.mkdir(exist_ok=True, parents=True)
+                adapter_file_path = adapter_dir_path.joinpath(module_name+'.py')
+                with open(adapter_file_path, 'w') as adapter_file:
+                    templateLoader = jinja2.FileSystemLoader(str(Path(__file__).parent))
+                    templateEnv = jinja2.Environment(loader=templateLoader)
+                    TEMPLATE_FILE = "pycompss_wrapper.tmpl"
+                    template = templateEnv.get_template(TEMPLATE_FILE)
+                    adapter_file.write(template.render(module_info=module_info))
+
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Creates pycompss adapters.",
+                                     formatter_class=lambda prog: argparse.RawTextHelpFormatter(prog, width=99999),
+                                     epilog='''Examples: \npycompss_generator.py -p biobb_package  -o path/to/biobb_adapters/biobb_adapters/pycompss\ncwl_generator.py --package biobb_package --input path/to/json_schemas --output path/to/cwl_adapters''')
+    required_args = parser.add_argument_group('required arguments')
+    required_args.add_argument('--package_name', '-p', required=True, help='BioBB package name from where the json schemas will be taken.')
+    required_args.add_argument('--output_pycompss_dir', '-o', required=True, help='Output path to the Pycompss adapters folder.')
+    args = parser.parse_args()
+
+    PycompssGenerator(package=args.package_name, output_dir=args.output_pycompss_dir).launch()
+
+
+if __name__ == '__main__':
+    main()
